2be38e3ea23c6fe797d57a2bc012edcb64ab6730
Balazs Kocso <kocsob@users.noreply.github.com>
2015-11-15 23:26:14 +0100

    deleteSpending middleware implemented

--- middleware/spending/deleteSpending.js
+++ middleware/spending/deleteSpending.js
@@ -1,14 +1,27 @@
 var requireOption = require('../common').requireOption;
 
 /**
- * This middleware deletes one spending to the model
+ * This middleware deletes one spending from the model
  */
 module.exports = function (objectRepository) {
 
     var spendingModel = requireOption(objectRepository, 'spendingModel');
 
     return function (req, res, next) {
-        return next();
+        // Check the _id if not exists send Bad Request
+        if (typeof req.body._id === 'undefined') {
+            return res.status(400).end();
+        }
+
+        // Delete spending from the DB
+        spendingModel.remove({
+            _id: req.body._id
+        }, function (err) {
+            if (err) {
+                next(err);
+            }
+            return res.status(200).end();
+        });
     };
 
 };
