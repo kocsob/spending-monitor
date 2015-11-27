4d243ab68fbf8cfc309b4e28bcc0af6813dfcb90
Balazs Kocso <kocsob@users.noreply.github.com>
2015-11-27 17:20:15 +0100

    Modify spending function added to the Spendings page

--- middleware/spending/modifySpending.js
+++ middleware/spending/modifySpending.js
@@ -1,14 +1,52 @@
 var requireOption = require('../common').requireOption;
 
 /**
- * This middleware modifies one spending to the model
+ * This middleware modifies one spending in the model
  */
 module.exports = function (objectRepository) {
 
     var spendingModel = requireOption(objectRepository, 'spendingModel');
 
     return function (req, res, next) {
-        return next();
+        var data = {};
+
+        // Check the _id if not exists send Bad Request
+        if (typeof req.body._id === 'undefined') {
+            return res.status(400).end();
+        }
+
+        // Collect the update information
+        if (typeof req.body.date !== 'undefiend') {
+            data.date = req.body.date;
+        }
+        if (typeof req.body.item !== 'undefiend') {
+            data.item = req.body.item;
+        }
+        if (typeof req.body.category !== 'undefiend') {
+            data.category = req.body.category;
+        }
+        if (typeof req.body.amount !== 'undefiend') {
+            data.amount = req.body.amount
+        }
+
+        // If no update information present send Bad Request
+        if (Object.keys(data).length == 0) {
+            return res.status(400).end();
+        }
+
+        // Update information in the DB
+        spendingModel.update({
+            _id: req.body._id
+        }, {
+            $set: data
+        }, function(err, numAffected) {
+            if (err) {
+                next(err);
+            }
+            // numAffected is the number of updated documents
+
+            res.status(200).end();
+        });
     };
 
 };
--- public/js/spendings.js
+++ public/js/spendings.js
@@ -5,58 +5,30 @@ angular.module('spendingsApp', ['ui.bootstrap'])
     }])
 
     .controller('spendingsCtrl', ['$scope', '$http', function($scope, $http) {
-        $scope.status = {
-            datePickerOpened: false,
-            monthPickerOpened: false
+        $scope.spendingCategories = [
+            "Food",
+            "Entertainment",
+            "Medical",
+            "Other"
+        ];
+
+        $scope.datapickerOpened = {
+            addSpending: false,
+            modifySpending: false,
+            monthPicker: false
         };
 
-        $scope.formdata = {
+        $scope.addSpendingData = {
             date: new Date(),
-            category: "Other"
+            category: $scope.spendingCategories[$scope.spendingCategories.length - 1]
         };
+
+        $scope.modifySpendingData = {};
 
         $scope.spendingMonth = new Date();
         $scope.monthChanged = getSpending;
 
         getSpending();
-
-        $scope.addSpending = function () {
-            var data = {
-                amount: $scope.formdata.amount,
-                category: $scope.formdata.category,
-                date: $scope.formdata.date,
-                item: $scope.formdata.item
-            };
-
-            $http.post('/spendings/add', data).success(function (result) {
-                data._id = result._id;
-                $scope.spendings.push(data);
-                $scope.total_amount = totalAmount($scope.spendings);
-            });
-        };
-
-        $scope.deleteSpending = function (spending) {
-            $http.post('/spendings/delete', {_id: spending._id}).success(function () {
-                $scope.spendings.splice($scope.spendings.indexOf(spending),1);
-                $scope.total_amount = totalAmount($scope.spendings);
-            });
-        };
-
-        $scope.openDatePicker = function($event) {
-            $scope.status.datePickerOpened = true;
-        };
-
-        $scope.openMonthPicker = function($event) {
-            $scope.status.monthPickerOpened = true;
-        };
-
-        function totalAmount(spendings) {
-            var total_amount = 0;
-            spendings.forEach(function (spending) {
-                total_amount += spending.amount;
-            });
-            return total_amount;
-        }
 
         function getSpending() {
             var date = $scope.spendingMonth;
@@ -72,6 +44,70 @@ angular.module('spendingsApp', ['ui.bootstrap'])
                 $scope.spendings = result;
                 $scope.total_amount = totalAmount(result);
             });
+        }
+
+        $scope.addSpending = function () {
+            var data = {
+                amount: $scope.addSpendingData.amount,
+                category: $scope.addSpendingData.category,
+                date: $scope.addSpendingData.date,
+                item: $scope.addSpendingData.item
+            };
+
+            $http.post('/spendings/add', data).success(function (result) {
+                getSpending();
+            });
+        };
+
+        $scope.modifySpending = function (spending) {
+            $scope.modifySpendingData = spending;
+        };
+
+        $scope.saveModifying = function () {
+            var data = {
+                _id: $scope.modifySpendingData._id,
+                amount: $scope.modifySpendingData.amount,
+                category: $scope.modifySpendingData.category,
+                date: $scope.modifySpendingData.date,
+                item: $scope.modifySpendingData.item
+            };
+
+            $http.post('/spendings/modify', data).success(function (result) {
+                getSpending();
+                $scope.modifySpendingData = {};
+            });
+        };
+
+        $scope.cancelModifying = function () {
+            getSpending();
+            $scope.modifySpendingData = {};
+        };
+
+        $scope.deleteSpending = function (spending) {
+            $http.post('/spendings/delete', {_id: spending._id}).success(function () {
+                $scope.spendings.splice($scope.spendings.indexOf(spending),1);
+                $scope.total_amount = totalAmount($scope.spendings);
+            });
+        };
+
+        $scope.openAddSpendingDatePicker = function($event) {
+            $scope.datapickerOpened.addSpending = true;
+        };
+
+        $scope.openModifySpendingDatePicker = function($event) {
+            $scope.datapickerOpened.modifySpending = true;
+        };
+
+        $scope.openMonthPicker = function($event) {
+            $scope.datapickerOpened.monthPicker = true;
+        };
+
+        function totalAmount(spendings) {
+            var total_amount = 0;
+            spendings.forEach(function (spending) {
+                total_amount += spending.amount;
+            });
+            return total_amount;
         }
 
 }]);
